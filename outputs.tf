d3d5826815d1d109ca7b43d48d594d9bfb52f3b0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,18 @@
-output "bot_channel_lines" {
-  description = "All bot_channel_line resources"
-  value       = azurerm_bot_channel_line.bot_channel_lines
-  sensitive   = true
-}
 output "bot_channel_lines_bot_name" {
-  description = "List of bot_name values across all bot_channel_lines"
-  value       = [for k, v in azurerm_bot_channel_line.bot_channel_lines : v.bot_name]
+  description = "Map of bot_name values across all bot_channel_lines, keyed the same as var.bot_channel_lines"
+  value       = { for k, v in azurerm_bot_channel_line.bot_channel_lines : k => v.bot_name }
 }
 output "bot_channel_lines_line_channel" {
-  description = "List of line_channel values across all bot_channel_lines"
-  value       = [for k, v in azurerm_bot_channel_line.bot_channel_lines : v.line_channel]
+  description = "Map of line_channel values across all bot_channel_lines, keyed the same as var.bot_channel_lines"
+  value       = { for k, v in azurerm_bot_channel_line.bot_channel_lines : k => v.line_channel }
   sensitive   = true
 }
 output "bot_channel_lines_location" {
-  description = "List of location values across all bot_channel_lines"
-  value       = [for k, v in azurerm_bot_channel_line.bot_channel_lines : v.location]
+  description = "Map of location values across all bot_channel_lines, keyed the same as var.bot_channel_lines"
+  value       = { for k, v in azurerm_bot_channel_line.bot_channel_lines : k => v.location }
 }
 output "bot_channel_lines_resource_group_name" {
-  description = "List of resource_group_name values across all bot_channel_lines"
-  value       = [for k, v in azurerm_bot_channel_line.bot_channel_lines : v.resource_group_name]
+  description = "Map of resource_group_name values across all bot_channel_lines, keyed the same as var.bot_channel_lines"
+  value       = { for k, v in azurerm_bot_channel_line.bot_channel_lines : k => v.resource_group_name }
 }
 
